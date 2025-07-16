9eb5e0ffa803d305d506c8827069351b4393fecd
Oluwatobiloba Okekunle <okekunletobi@gmail.com>
2025-07-16 12:37:18 +0100

    added student ID to registration and implementing methods for setting student status and retrieving students by ID.

--- submissions/week-2/day-2/oluwatobiloba-okekunle/task-1/school-management-system/src/lib.rs
+++ submissions/week-2/day-2/oluwatobiloba-okekunle/task-1/school-management-system/src/lib.rs
@@ -9,6 +9,7 @@ pub enum StudentStatus {
 
 #[derive(Debug)]
 pub struct Student {
+  pub id: u32,
   pub name: String,
   pub grade: u32,
   pub gender: String,
@@ -25,8 +26,8 @@ impl School {
     Self { students: Vec::new() }
   }
 
-  pub fn register_student(&mut self, name: String, grade: u32, gender: String) {
-    let student = Student { name, grade, gender, status: StudentStatus::ACTIVE };
+  pub fn register_student(&mut self, id: u32, name: String, grade: u32, gender: String) {
+    let student = Student { id, name, grade, gender, status: StudentStatus::ACTIVE };
     self.students.push(student);
   }
 
@@ -51,6 +52,20 @@ impl School {
 
   pub fn delete_student(&mut self, index: usize) {
     self.students.remove(index);
+  }
+
+  pub fn set_student_status(&mut self, id: u32, status: StudentStatus) -> bool {
+    if let Some(student) = self.students.iter_mut().find(|s| s.id == id) {
+      println!("Student found: {:?}", student);
+      student.status = status;
+      true
+    } else {
+      false
+    }
+  }
+
+  pub fn get_student_by_id(&self, id: u32) -> Option<&Student> {
+    self.students.iter().find(|s| s.id == id)
   }
   
 }
--- submissions/week-2/day-2/oluwatobiloba-okekunle/task-1/school-management-system/src/main.rs
+++ submissions/week-2/day-2/oluwatobiloba-okekunle/task-1/school-management-system/src/main.rs
@@ -1,10 +1,10 @@
-use school_management_system::{School}; // adjust as needed
+use school_management_system::{School, StudentStatus};
 
 fn main() {
     let mut school = School::new();
-    school.register_student(String::from("John"), 10, String::from("male"));
-    school.register_student(String::from("Jane"), 11, String::from("female"));
-    school.register_student(String::from("Jim"), 12, String::from("male"));
+    school.register_student(1, String::from("John"), 10, String::from("male"));
+    school.register_student(2, String::from("Jane"), 11, String::from("female"));
+    school.register_student(3, String::from("Jim"), 12, String::from("male"));
 
 
     println!("All students: {:?}", school.get_all_students());
@@ -29,7 +29,7 @@ fn main() {
 
 
 
-    school.register_student(String::from("Akin"), 12, String::from("male"));
+    school.register_student(4, String::from("Akin"), 12, String::from("male"));
 
     
     // delete student by index
@@ -47,4 +47,12 @@ fn main() {
     // get student by name
     let student_by_name = school.get_student_by_name(String::from("Akin"));
     println!("Student by name: {:?}", student_by_name);
+
+
+    // set student status
+    let status = school.set_student_status(2, StudentStatus::INACTIVE);
+    println!("Student status: {:?}", status);
+    // print student with id 1
+    let student = school.get_student_by_id(2);
+    println!("Student with id 1: {:?}", student);
 }
