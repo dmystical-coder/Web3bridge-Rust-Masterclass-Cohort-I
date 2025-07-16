c665dc58e9cf825d6917dc30952796a22da0e63a
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2025-07-16 23:32:37 +0100

    uchee

--- submissions/week-2/day-2/Ekezie-Uchenna/sms/src/lib.rs
+++ submissions/week-2/day-2/Ekezie-Uchenna/sms/src/lib.rs
@@ -4,6 +4,7 @@ use std::fmt::Display;
 
 #[derive(Debug)]
 struct Student{
+    id: u32,
     name: String,
     grade: u8,
     status: Status
@@ -38,22 +39,39 @@ impl School{
         }
     }
 
-    pub fn update_status(&mut self, name: String, status: Status){
-        for student in &mut self.students{
-            if student.name == name{
-                student.status = status.clone();
-            }
+    pub fn update_status(&mut self, id: u32, status: Status){
+      if let Some(student) = self.students.iter_mut().find(|s| s.id == id) {
+            student.status = status;
+        } else {
+            println!("Student not found");
         }
     }
 
-    pub fn delete_student(&mut self, name: String){
-        for student in &mut self.students{
-            if student.name == name{
-                student.status = Status::Inactive;
-            }
+    pub fn delete_student(&mut self, id: u32){
+        if let Some(student) = self.students.iter_mut().find(|s| s.id == id) {
+            student.status = Status::Inactive;
+        } else {
+            println!("Student not found");
         }
     }
 
+   
+    pub fn get_student_by_id_and_set_status(&mut self, id: u32, status: Status){
+
+        if let Some(student) = self.students.iter_mut().find(|s| s.id == id) {
+            match student.status {
+                Status::Active => {
+                    student.status = status.clone();
+                },
+                Status::Inactive => {
+                    println!("Student is already inactive");
+                }
+
+            }
+        } else {
+            println!("Student not found");
+        }
+    }
     pub fn view_students(&self){
         for student in &self.students{
             println!("Name: {:?}, Grade: {:?}, Status: {:?}", student.name, student.grade, student.status);
@@ -69,6 +87,7 @@ mod test {
     fn test_register_student(){
         let mut school = School::new();
         let student = Student{
+            id: 1,
             name: String::from("Ekezie Uchenna"),
             grade: 10,
             status: Status::Active
@@ -81,6 +100,7 @@ mod test {
     fn test_edit_student(){
         let mut school = School::new();
         let student = Student{
+            id: 1,
             name: String::from("Ekezie Uchenna"),
             grade: 10,
             status: Status::Active
@@ -92,15 +112,16 @@ mod test {
 
      #[test]
 
-     fn test_update_student(){
+     fn test_update_status(){
         let mut school = School::new();
         let student = Student{
+            id: 1,
             name: String::from("Ekezie Uchenna"),
             grade: 10,
             status: Status::Active
         };
         school.register_student(student);
-        school.update_status(String::from("Ekezie Uchenna"), Status::Inactive);
+        school.update_status(1, Status::Inactive);
         assert_eq!(school.students[0].status, Status::Inactive);
      }
 
@@ -109,23 +130,26 @@ mod test {
      fn test_delete_student(){
         let mut school = School::new();
         let student = Student{
+            id: 1,
             name: String::from("Ekezie Uchenna"),
             grade: 10,
             status: Status::Active
         };
         school.register_student(student);
-        school.delete_student(String::from("Ekezie Uchenna"));
+        school.delete_student(1);
         assert_eq!(school.students[0].status, Status::Inactive);
      }
 #[test]
      fn test_view_student(){
         let mut school = School::new();
         let student1 = Student{
+            id: 1,
             name: String::from("Ekezie Uchenna"),
             grade: 10,
             status: Status::Active
         };
         let student2 = Student{
+            id: 2,
             name: String::from("Josh Tutor"),
             grade: 20,
             status: Status::Inactive
@@ -135,5 +159,19 @@ mod test {
 
         school.view_students();
      }
+
+    #[test]
+    fn test_get_student_by_id_and_set_status(){
+        let mut school = School::new();
+        let student = Student{
+            id: 1,
+            name: String::from("Ekezie Uchenna"),
+            grade: 10,
+            status: Status::Active
+        };
+        school.register_student(student);
+        school.get_student_by_id_and_set_status(1, Status::Inactive);
+        assert_eq!(school.students[0].status, Status::Inactive);
+    }
 }
 
