4aa20714875e0e248930b2802c83213aa9667cfc
mac <arowolokehinde321@gmail.com>
2025-07-17 09:50:56 +0100

    Web3bridge aceess control task week 2

--- submissions/week-2/day-3/kehinde-arowolo-day3/src/main.rs
+++ submissions/week-2/day-3/kehinde-arowolo-day3/src/main.rs
@@ -1,5 +1,3 @@
-use std::fmt;
-
 #[derive(Debug, Clone)]
 enum EmployeeType {
     Media,
@@ -12,6 +10,7 @@ enum EmployeeType {
 
 #[derive(Debug)]
 struct Employee {
+    name: String,
     employee_type: EmployeeType,
     is_employed: bool,
 }
@@ -22,11 +21,32 @@ enum AccessError {
     Unauthorized,
 }
 
-impl fmt::Display for AccessError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+impl AccessError {
+    fn message(&self) -> &str {
         match self {
-            AccessError::Terminated => write!(f, "Access denied: Employee is terminated"),
-            AccessError::Unauthorized => write!(f, "Access denied: Employee role is not authorized"),
+            AccessError::Terminated => " Access denied: Employee is terminated",
+            AccessError::Unauthorized => "Access denied: Employee role is not authorized for garage access",
+        }
+    }
+}
+
+impl Employee {
+    fn new(name: &str, employee_type: EmployeeType, is_employed: bool) -> Self {
+        Employee {
+            name: name.to_string(),
+            employee_type,
+            is_employed,
+        }
+    }
+    
+    fn role_name(&self) -> &str {
+        match self.employee_type {
+            EmployeeType::Media => "Media Team",
+            EmployeeType::ITDepartment => "IT Department",
+            EmployeeType::Manager => "Manager",
+            EmployeeType::SocialMedia => "Social Media Team",
+            EmployeeType::Technician => "Technician Supervisor",
+            EmployeeType::KitchenStaff => "Kitchen Staff",
         }
     }
 }
@@ -37,9 +57,7 @@ fn check_building_access(employee: &Employee) -> Result<(), AccessError> {
     }
     
     match employee.employee_type {
-        EmployeeType::Media | EmployeeType::ITDepartment | EmployeeType::Manager => {
-            Ok(())
-        }
+        EmployeeType::Media | EmployeeType::ITDepartment | EmployeeType::Manager => Ok(()),
         EmployeeType::SocialMedia | EmployeeType::Technician | EmployeeType::KitchenStaff => {
             Err(AccessError::Unauthorized)
         }
@@ -47,45 +65,32 @@ fn check_building_access(employee: &Employee) -> Result<(), AccessError> {
 }
 
 fn print_access_status(employee: &Employee) -> Result<(), AccessError> {
-    check_building_access(employee)?; 
-    println!(" Employee can access the web3bridge garage");
+    check_building_access(employee)?;
+    println!("{} ({}) can access the web3bridge garage", employee.name, employee.role_name());
     Ok(())
 }
 
 fn main() {
-
     let employees = vec![
-        Employee {
-            employee_type: EmployeeType::Media,
-            is_employed: true,
-        },
-        Employee {
-            employee_type: EmployeeType::ITDepartment,
-            is_employed: true,
-        },
-        Employee {
-            employee_type: EmployeeType::Manager,
-            is_employed: true,
-        },
-        Employee {
-            employee_type: EmployeeType::SocialMedia,
-            is_employed: true,
-        },
-        Employee {
-            employee_type: EmployeeType::Technician,
-            is_employed: true,
-        },
-        Employee {
-            employee_type: EmployeeType::KitchenStaff,
-            is_employed: true,
-        },
+        Employee::new("Arowolo", EmployeeType::Media, true),
+        Employee::new("Vicent", EmployeeType::ITDepartment, true),
+        Employee::new("David", EmployeeType::Manager, true),
+        Employee::new("Acheiver", EmployeeType::SocialMedia, true),
+        Employee::new("Fuhad", EmployeeType::Technician, true),
+        Employee::new("Frank", EmployeeType::KitchenStaff, true),
+        Employee::new("Ayomide", EmployeeType::Manager, false), // Terminated
     ];
 
-    for employee in employees {
-        println!("\nTesting {:?} employee:", employee.employee_type);
-        match print_access_status(&employee) {
+    println!("Web3Bridge Garage Access Control");
+
+    for employee in &employees {
+        println!("\n Testing: {} ({})", employee.name, employee.role_name());
+        println!("   Status: {}", if employee.is_employed { " Active" } else { " Terminated" });
+        
+        match print_access_status(employee) {
             Ok(()) => {},
-            Err(e) => println!("{}", e),
+            Err(e) => println!("   {}", e.message()),
         }
     }
 }
+
