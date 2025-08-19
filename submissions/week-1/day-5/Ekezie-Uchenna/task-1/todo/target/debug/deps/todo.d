a9c2e981356cb3cd6aa9a7b5e48eca392bac5672
Uchechukwu <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2025-08-19 03:58:29 -0700

    q

--- submissions/week-1/day-5/Ekezie-Uchenna/task-1/todo/target/debug/deps/todo.d
+++
@@ -1,5 +0,0 @@
-D:\linuc\uche\Web3bridge-Rust-Masterclass-Cohort-I\submissions\week-1\day-5\Ekezie-Uchenna\task-1\todo\target\debug\deps\todo.d: src\main.rs
-
-D:\linuc\uche\Web3bridge-Rust-Masterclass-Cohort-I\submissions\week-1\day-5\Ekezie-Uchenna\task-1\todo\target\debug\deps\todo.exe: src\main.rs
-
-src\main.rs:
