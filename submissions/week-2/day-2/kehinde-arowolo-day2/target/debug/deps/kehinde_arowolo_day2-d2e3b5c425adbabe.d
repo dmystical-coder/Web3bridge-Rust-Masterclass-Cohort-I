f1deb7d5f0c2aec1259e982ad5ad460cd69c82d0
mac <arowolokehinde321@gmail.com>
2025-08-19 10:38:04 +0100

    removed target folder

--- submissions/week-2/day-2/kehinde-arowolo-day2/target/debug/deps/kehinde_arowolo_day2-d2e3b5c425adbabe.d
+++
@@ -1,5 +0,0 @@
-/Users/mac/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/kehinde-arowolo-day2/target/debug/deps/libkehinde_arowolo_day2-d2e3b5c425adbabe.rmeta: src/main.rs
-
-/Users/mac/Web3bridge-Rust-Masterclass-Cohort-I/submissions/week-2/day-2/kehinde-arowolo-day2/target/debug/deps/kehinde_arowolo_day2-d2e3b5c425adbabe.d: src/main.rs
-
-src/main.rs:
