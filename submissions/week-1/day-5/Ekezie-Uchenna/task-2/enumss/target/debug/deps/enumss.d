1b5115f41d49c96196c747b425d0eb814e1b04f5
Uchechukwu <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2025-08-19 03:59:37 -0700

    q

--- submissions/week-1/day-5/Ekezie-Uchenna/task-2/enumss/target/debug/deps/enumss.d
+++
@@ -1,5 +0,0 @@
-D:\linuc\uche\Web3bridge-Rust-Masterclass-Cohort-I\submissions\week-1\day-5\Ekezie-Uchenna\task-2\enumss\target\debug\deps\enumss.d: src\main.rs
-
-D:\linuc\uche\Web3bridge-Rust-Masterclass-Cohort-I\submissions\week-1\day-5\Ekezie-Uchenna\task-2\enumss\target\debug\deps\enumss.exe: src\main.rs
-
-src\main.rs:
